efc8d338998428cf5baefd58cc0bb44253b7a431
Mark Charlebois <charlebm@gmail.com>
2016-01-09 19:13:29 -0800

    Require DO_JIG_TEST for GPIO INT test
    The test only succeeds when it is connected to a test jig.
    
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- test/dspal_tester/adsp_proc/gpio_test_imp.c
+++ test/dspal_tester/adsp_proc/gpio_test_imp.c
@@ -265,9 +265,11 @@ void *gpio_int_isr(DSPAL_GPIO_INT_ISR_CTX context)
 * @return
 * TEST_PASS ------ Test Passes
 * TEST_FAIL ------ Test Failed
+* TEST_SKIP ------ Test Skipped
 */
 int dspal_tester_test_gpio_int(void)
 {
+#ifdef DO_JIG_TEST
    int result = TEST_PASS;
    enum DSPAL_GPIO_VALUE_TYPE value_written;
    int fd;
@@ -353,5 +355,8 @@ int dspal_tester_test_gpio_int(void)
 exit:
    close(int_fd);
    close(fd);
+#else
+   int result = TEST_SKIP;
+#endif
    return result;
 }
