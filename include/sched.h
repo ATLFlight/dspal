d6939cbae497159dadfbf8261b1919444a6734b7
jwilson <jwilson@qti.qualcomm.com>
2016-02-25 18:26:37 -0800

    Removing unsupported functions defined in sched.h

--- include/sched.h
+++ include/sched.h
@@ -87,12 +87,6 @@ struct timespec;
 __BEGIN_DECLS
 int     sched_get_priority_max(int);
 int     sched_get_priority_min(int);
-int     sched_getparam(pid_t, struct sched_param *);
-int     sched_getscheduler(pid_t);
-int     sched_rr_get_interval(pid_t, struct timespec *);
-int     sched_setparam(pid_t, const struct sched_param *);
-int     sched_setscheduler(pid_t, int, const struct sched_param *);
-int     sched_yield(void);
 __END_DECLS
 
 #endif /* !_SCHED_H_ */
