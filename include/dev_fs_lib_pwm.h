e7c9d42b82bf54ab094176ace20fe0d6fff56f0b
jwilson <jwilson@qti.qualcomm.com>
2016-06-10 05:33:14 -0700

    Optimizing API to allow direct access to the buffer used in controlling GPIO state.

--- include/dev_fs_lib_pwm.h
+++ include/dev_fs_lib_pwm.h
@@ -75,18 +75,23 @@
 enum DSPAL_PWM_IOCTLS {
 	PWM_IOCTL_INVALID = -1, /**< invalid IOCTL code, used to return an error */                                 //!< PWM_IOCTL_INVALID
 	PWM_IOCTL_SIGNAL_DEFINITION, /**< used to define the gpio number(s) and period of the pulse width(s) */     //!< PWM_IOCTL_SIGNAL_DEFINITION
-	PWM_IOCTL_PULSE_WIDTH, /**< used to define the width of each pulse defined in PWM_IOCTL_SIGNAL_DEFINITION *///!< PWM_IOCTL_PULSE_WIDTH
+	PWM_IOCTL_GET_UPDATE_BUFFER, /**< returns a buffer used to update the pulse width in real-time */      //!< PWM_IOCTL_GET_PULSE_WIDTH_BUFFER
 	PWM_IOCTL_MAX_NUM, /**< number of valid IOCTL codes defined for the PWM generator */                        //!< PWM_IOCTL_MAX_NUM
 };
 
 /**
  * @brief
- * Structure used to define the ID of the I/O line and the width of the pulse
- * used by the signal generator.
+ * Structure used to define the GPIO ID of the I/O line and the width of the pulse
+ * used by the signal generator.  This structure is also used to return a buffer containing
+ * an array of structures that can be accessed directly to effect a change in the width
+ * of the pulse.  @see PWM_IOCTL_GET_UPDATE_BUFFER
+ *
  */
-struct dspal_pwm_signal {
-	uint32_t gpio_id;
-	uint32_t pulse_width_in_usecs;
+struct dspal_pwm {
+	uint32_t gpio_id;               /**< ID of the GPIO line used for PWM generation */
+	uint32_t gpio_cfg;              /**< internal use only */
+	uint32_t pulse_width_in_usecs;  /**< duration of the pulse in usecs */
+	uint32_t pulse_state;           /**< a read-only value, indicating the current state of the pulse */
 };
 
 /**
@@ -99,23 +104,28 @@ struct dspal_pwm_signal {
 struct dspal_pwm_ioctl_signal_definition {
 	uint32_t period_in_usecs; /**< the period of the pulses generated, maximum period [TODO-JYW], can only be changed once after the device is first opened */
 	uint32_t num_signals; /**< number of signals specified in the following array, maximum number [TODO-JYW] */
-	struct dspal_pwm_signal *pwm_signals; /**< array defining the GPIO lines and pulse widths to be used by the signal generator */
+	struct dspal_pwm *pwm_signals; /**< array defining the GPIO lines and pulse widths to be used by the signal generator */
 };
 
 /**
  * @brief
- * Structure used in the ioctl: PWM_IOCTL_PULSE_WIDTH
+ * Structure used in the ioctl: PWM_IOCTL_GET_UPDATE_BUFFER
  *
- * At the end of the current period (defined with PWM_IOCTL_SIGNAL_DEFINITION), the pulses
- * specified will be generated for each I/O line.  Pulses specified with PWM_IOCTL_SIGNAL_DEFINITION,
- * but NOT included in the pwm_signals array will remain unchanged and continue to generated pulses
- * as specified.
+ * Returns a buffer that can be used to effect an immediate change in the width of the pulse.
+ * @par
+ * To change the width of a particular pulse the new pulse width should be written to the
+ * pulse_width_in_usecs structure member.  If the deadline for changing the state of the pulse
+ * has already passed, or the width of the pulse is less than the previous pulse width the
+ * new value will not take effect until the next period.
+ *
+ * No other structure member other than pulse_width_in_usecs may be modified.  To use different
+ * GPIO lines or change the period a new signal definition must be created.
  *
  * The I/O lines used cannot be changed from those defined in in the PWM_IOCTL_SIGNAL_DEFINITION structure.
  */
 struct dspal_pwm_ioctl_pulse_width {
 	uint32_t num_signals; /**< the number of signals specified in the following array */
-	struct dspal_pwm_signal *pwm_signals; /**< array defining the GPIO lines and pulse widths to be used by the signal generator, can only be specified once after the device is first opened */
+	struct dspal_pwm *pwm_signals; /**< array defining the GPIO lines and pulse widths to be used by the signal generator, can only be specified once after the device is first opened */
 };
 
 /**
